b76cc6f7e3a126036aa7ffaa4f3ad527055efb9a
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-06-04 22:35:04 +0200

    major update

--- src/registry.lua
+++ src/registry.lua
@@ -73,44 +73,80 @@ local split = function(str, pat)
   return t
 end
 
-local function parsequery(output)
-  local lines = split(output, "\n")
-  local i = 1
+-- wrap string in double quotes
+local dqwrap = function(str)
+  assert(type(str)=="string", "Expected string, got "..type(str))
+  return '"'..str..'"'
+end
+
+-- returns a tree structure with the registry key
+-- the key-table has fields:
+--   key    : the full key, eg. "HKEY_CLASSES_ROOT\somekey\subkey\subsubkey"
+--   keys   : table (indexed by name) with key-table of the sub key (same structure)
+--   values : table (indexed by name) with a values-table having fields:
+--          value: the value
+--          type : the registry value type, eg. REG_SZ, REG_MULTI_SZ, etc.
+--          name : the name
+local function parsequery(output, i)
+  assert(type(output) == "string" or type(output) == "table", "Expected string or table, got "..type(output))
+  local lines
+  if type(output) == "string" then 
+    lines = split(output, "\n")
+  else
+    lines = output
+  end
+  local i = i or 1
   local result = { values = {}, keys = {} }
   while i <= #lines do
     if lines[i] ~= "" then
       if result.key then 
         -- key already set, so this is content
         if lines[i]:find(result.key,1,true) == 1 then
-          -- the line starts with the same sequnce as our key, so it is a sub-key
-          table.insert(result.keys, lines[i])
-          result.keys[lines[i]] = #result.keys
+          -- the line starts with the same sequence as our key, so it is a sub-key
+          local skey
+          local name = lines[i]:sub(#result.key + 2, -1)
+          skey, i = parsequery(lines, i)
+          result.keys[name] = skey
         else
           -- not a sub key, so it is a value
           local n, t, v = lines[i]:match("^%s%s%s%s(.+)%s%s%s%s(REG_.+)%s%(%d%d?%)%s%s%s%s(.*)$")
-if not n then print("fialed: '"..lines[i].."'"          ) end
           result.values[n] = { ["type"] = t, value = v, name = n}
-          table.insert(result.values, result.values[n])
         end
       else
         -- key not set, so this is the key
         result.key = lines[i]
       end
+    else
+      if result.key then
+        -- blank line while key already set, so we're done with the values
+        while lines[i] == "" and i <= #lines do i = i + 1 end
+        if lines[i] then
+          if lines[i]:find(result.key,1,true) ~= 1 then
+            -- the next key in the list is not a sub key, so we're done
+            return result, i
+          else
+            i = i - 1 
+          end
+        end
+      end
     end    
     i = i + 1
   end
   if result.key then
-    return result
+    return result, i
   else
     return nil
   end
 end
 
---- returns all values of a key.
+--- Returns the contents of a registry key.
 -- @param key full key eg. "HKLM\SOFTWARE\xPL"
-function registry.getvalues(key)
+-- @param recursive if truthy, then a recursive tree will be generated will all sub keys as well
+function registry.getkey(key, recursive)
   assert(type(key)=="string", "Expected string, got "..type(key))
-  local ok, ec, out, err = executeex([[reg.exe query "]]..key..[[" /z]])
+  local options = " /z"
+  if recursive then options = options.." /s" end
+  local ok, ec, out, err = executeex([[reg.exe query ]]..dqwrap(key)..options)
   if not ok then 
     return nil, err
   else
@@ -118,16 +154,78 @@ function registry.getvalues(key)
   end
 end
 
-local o = registry.getvalues([[hkcr\..test]],"dword32")
-print("key = ", o.key)
-for k,v in ipairs(o.values) do
-  print("name       :", v.name)
-  print("    type   :", v.type)
-  print("    value  :", v.value)
+-- creates a key
+-- @return true on success, nil+err on failure
+function registry.createkey(key)
+  local ok, ec, out, err = executeex([[reg.exe add ]]..dqwrap(key))
+  if not ok then
+    return nil, err
+  else
+    return true
+  end
 end
-print("sub keys:")
-for k,v in ipairs(o.keys) do
-  print("    "..k.." :", v)
+
+--- deletes a key (and all of its contents)
+-- @return true on success, nil+err on failure (deleting a non-existing key returns success)
+function registry.deletekey(key)
+  local ok, ec, out, err = executeex([[reg.exe delete ]]..dqwrap(key).. [[ /f]])
+  if not ok then
+    if not registry.getkey(key) then return true end -- it didn't exist in the first place
+    return nil, err
+  else
+    return true
+  end
+end
+
+--- write a value (will create the key in the process)
+-- will overwrite existing values without prompt
+-- use name = "(Default)" for default value
+-- @return true on success, nil+err on failure
+function registry.writevalue(key, name, vtype, value)
+  local command
+  if name == "(Default)" or name == nil then 
+    command = ("reg.exe add %s /ve /t %s /d %s /f"):format(dqwrap(key), vtype, dqwrap(value))
+  else
+    command = ("reg.exe add %s /v %s /t %s /d %s /f"):format(dqwrap(key),dqwrap(name), vtype, dqwrap(value))
+  end
+  local ok, ec, out, err = executeex(command)
+  if not ok then
+    return nil, err
+  else
+    return true
+  end
+end
+
+--- deletes a value
+-- use name = "(Default)" for default value, or name = nil
+-- @return true on success, nil+err on failure (deleting a non-existing value returns success)
+function registry.deletevalue(key, name)
+  local command
+  if name == "(Default)" or name == nil then 
+    command = ("reg.exe delete %s /ve /f"):format(dqwrap(key))
+  else
+    command = ("reg.exe delete %s /v %s /f"):format(dqwrap(key),dqwrap(name))
+  end
+  local ok, ec, out, err = executeex(command)
+  if not ok then
+    if not registry.getvalue(key, name) then return true end -- it didn't exist in the first place
+    return nil, err
+  else
+    return true
+  end
+end
+
+--- Returns a value.
+-- return value, type or nil if it doesn't exist
+function registry.getvalue(key, name)
+  local keyt = registry.getkey(key)
+  if keyt then
+    if keyt[name] then
+      -- it exists, return value and type
+      return keyt[name].value, keyt[name].type
+    end
+  end
+  return nil
 end
 
 return registry
